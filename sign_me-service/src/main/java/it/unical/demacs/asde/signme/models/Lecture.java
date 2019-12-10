010592c4fcd5ab38dee23297b1d20b896592ce8e
peace27-96 <pace27.01@gmail.com>
2019-12-10 19:06:46 +0100

    class DAO added

---
+++ sign_me-service/src/main/java/it/unical/demacs/asde/signme/models/Lecture.java
@@ -0,0 +1,41 @@
+package it.unical.demacs.asde.signme.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Lecture")
+public class Lecture {
+
+	@Id
+	private Integer lectureId;
+	private String description;
+
+	public Lecture() {
+		super();
+	}
+
+	public Lecture(Integer lectureId, String description) {
+		super();
+		this.lectureId = lectureId;
+		this.description = description;
+	}
+
+	public Integer getLectureId() {
+		return lectureId;
+	}
+
+	public void setLectureId(Integer lectureId) {
+		this.lectureId = lectureId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ sign_me-service/src/main/java/it/unical/demacs/asde/signme/models/Material.java
@@ -0,0 +1,41 @@
+package it.unical.demacs.asde.signme.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Material")
+public class Material {
+
+	@Id
+	private Integer materialId;
+	private String description;
+
+	public Material() {
+		super();
+	}
+
+	public Material(Integer materialId, String description) {
+		super();
+		this.materialId = materialId;
+		this.description = description;
+	}
+
+	public Integer getMaterialId() {
+		return materialId;
+	}
+
+	public void setMaterialId(Integer materialId) {
+		this.materialId = materialId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
