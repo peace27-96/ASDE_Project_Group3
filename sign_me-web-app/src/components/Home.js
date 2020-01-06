a076474e4aed4b255176fb445288693b207d741b
peace27-96 <pace27.01@gmail.com>
2020-01-06 19:53:20 +0100

    a

--- sign_me-web-app/src/components/Home.js
+++ sign_me-web-app/src/components/Home.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
 import { useStyles } from './HomeStyle.js';
 import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import TabPanel from './TabPanel';
 import BaseInstance from '../http-client/BaseInstance'
 import CourseCreation from './CourseCreation'
+import Cookies from 'js-cookie'
+import { createBrowserHistory } from "history";
 
-export default function RecipeReviewCard(props) {
+export default function RecipeReviewCard({history}) {
+
+    if(history === undefined) {
+        history = createBrowserHistory()
+        history.push("/login")
+    }
+
+    if(Cookies.get("email") === undefined) 
+        history.push("/login");
+
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
+    Cookies.remove("currentCourse")
 
-    var imagePath = props.getPicture()
+    var imagePath = Cookies.get("profilePicture")
     if (imagePath === null) imagePath = '../resources/user-icon.png'
     else imagePath = 'http://localhost:8080/signme/' + imagePath
 
@@ -41,7 +37,7 @@ export default function RecipeReviewCard(props) {
         const formData = new FormData();
         var nameFile = e.target.files[0].name;
         var extension = nameFile.match(/.*\.(\w+)/)[1];
-        nameFile = props.getUser() + "." + extension.toLowerCase();
+        nameFile = Cookies.get("email") + "." + extension.toLowerCase();
         formData.append('file', e.target.files[0], nameFile);
         BaseInstance.post("updateProfilePicture", formData)
             .then(res => {
@@ -81,25 +77,25 @@ export default function RecipeReviewCard(props) {
                     <Card className={classes.summaryCard}>
                     <div style={{display:"flex","justify-content": "space-between"}}>
                         <Typography className={classes.summaryField}>Name: </Typography> 
-                        <Typography className={classes.summaryField}>{props.getCredentials().firstName}</Typography> 
+                        <Typography className={classes.summaryField}>{Cookies.get("firstName")}</Typography> 
                     </div>
                     <Divider></Divider>
                     <div style={{display:"flex","justify-content": "space-between"}}>
                         <Typography className={classes.summaryField}>Surname: </Typography> 
-                        <Typography className={classes.summaryField}>{props.getCredentials().lastName}</Typography> 
+                        <Typography className={classes.summaryField}>{Cookies.get("lastName")}</Typography> 
                     </div>
                     <Divider></Divider>
                     <div style={{display:"flex","justify-content": "space-between"}}>
                         <Typography className={classes.summaryField}>Email: </Typography> 
-                        <Typography className={classes.summaryField}>{props.getCredentials().email}</Typography> 
+                        <Typography className={classes.summaryField}>{Cookies.get("email")}</Typography> 
                     </div>
                     </Card>
-                    <CourseCreation getUser={props.getUser} addCourse={props.addCourse} />
+                    <CourseCreation/>
                 </Grid>
 
                 <Grid item xs={9} style={{ paddingLeft: "20px" }}>
                     <Card className={classes.coursesCard}>
-                        <TabPanel getCourses={props.getCourses} getSubscribedCourses={props.getSubscribedCourses} goToCoursePage={props.goToCoursePage} />
+                        <TabPanel />
                     </Card>
                 </Grid>
             </Grid>
